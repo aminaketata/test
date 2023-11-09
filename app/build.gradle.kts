ba2c6b647a3dcfb36543022b6a00f3d7cce0232d
Lenovo <amina.ketata00@gmail.com>
2023-11-09 13:34:50 +0100

    init1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.ketata_amina_tp2_personnaliser"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ketata_amina_tp2_personnaliser"
--- app/src/main/java/com/example/ketata_amina_tp2_personnaliser/MainActivity.java
+++ app/src/main/java/com/example/ketata_amina_tp2_personnaliser/MainActivity.java
@@ -3,12 +3,124 @@ package com.example.ketata_amina_tp2_personnaliser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etValeur;
+    private Button bConsulter;
+    private TextView tvAge,tvRésultat;
+    private RadioButton rbOui,rbNon;
+    private SeekBar sbAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        //listener SeekBar
+        sbAge.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+            {
+                Log.i("Information", "onProgressChanged " + i);
+                tvAge.setText("Votre âge : "+ i);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+        });
+        bConsulter.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Calculer();
+            }
+        });
+    }
+    private void init()
+    {
+        etValeur=(EditText) findViewById(R.id.etValeur);
+        sbAge=(SeekBar) findViewById(R.id.sbage);
+        tvAge=(TextView) findViewById(R.id.tvAge);
+        rbOui=(RadioButton) findViewById(R.id.rbtOui);
+        rbNon=(RadioButton) findViewById(R.id.rbtNon);
+        tvRésultat=(TextView) findViewById(R.id.tvRésultat);
+        bConsulter=(Button) findViewById(R.id.btnConsulter);
+    }
+    private void Calculer()
+    {
+        int age;
+        float valeurMesurer;
+        boolean verifAge=false,verifValeur=false;
+        if(sbAge.getProgress()!=0)
+            verifAge=true;
+        else
+            Toast.makeText(MainActivity.this, "veillez verifier votre age", Toast.LENGTH_SHORT).show();
+        if(!etValeur.getText().toString().isEmpty())
+            verifValeur=true;
+        else
+            Toast.makeText(MainActivity.this, "veillez verifier la valeur mesurer", Toast.LENGTH_LONG).show();
+        if (verifAge && verifValeur)
+        {
+            age = sbAge.getProgress();
+            valeurMesurer = Float.valueOf(etValeur.getText().toString());
+            boolean fasting = rbOui.isChecked();
+
+
+        if (fasting=true)
+        {
+            if (age>=13)
+            {
+                if (valeurMesurer<5)
+                    tvRésultat.setText("niveau de glycémie est bas");
+                else if (valeurMesurer>=5 && valeurMesurer<=7.2)
+                    tvRésultat.setText("niveau de glycémie est normale");
+                else
+                   tvRésultat.setText("niveau de glycémie est élevé");
+            }
+            else if (age>=6 && age<=12)
+            {
+                if (valeurMesurer<5)
+                    tvRésultat.setText("niveau de glycémie est bas");
+                else if (valeurMesurer>=5 && valeurMesurer<=10)
+                    tvRésultat.setText("niveau de glycémie est normale");
+                else
+                    tvRésultat.setText("niveau de glycémie est élevé");
+            }
+            else if (age<6)
+            {
+                if (valeurMesurer<5.5)
+                    tvRésultat.setText("niveau de glycémie est bas");
+                else if (valeurMesurer>=5.5 && valeurMesurer<=10)
+                    tvRésultat.setText("niveau de glycémie est normale");
+                else
+                    tvRésultat.setText("niveau de glycémie est élevé");
+            }
+        }
+        else
+        {
+            if (valeurMesurer> 10.5)
+                tvRésultat.setText("niveau de glycémie est élevé");
+            else
+                tvRésultat.setText("niveau de glycémie est normale");
+        }
+    }
     }
 }
+
